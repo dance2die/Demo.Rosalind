98656a09aa07ec93e843ace31e6427ef6cf685d0
dance2die <dontbothermewithspam@gmail.com>
2016-05-04 10:08:30 -0400

    Moved Dna class to its own file

--- Demo.Rosalind/Demo.Rosalind.Tests/DNA/DnaTest.cs
+++ Demo.Rosalind/Demo.Rosalind.Tests/DNA/DnaTest.cs
@@ -53,29 +53,4 @@ namespace Demo.Rosalind.Tests.DNA
 			_output.WriteLine(result);
 		}
 	}
-
-	public class Dna
-	{
-		private readonly string[] _dnaSymbols = { "A", "C", "G", "T" };
-
-		public string BuildOutputString(Dictionary<string, int> output)
-		{
-			var sortedOutput = new SortedDictionary<string, int>(output);
-			var values = sortedOutput.Select(pair => pair.Value.ToString());
-			return string.Join(" ", values);
-		}
-
-		public Dictionary<string, int> CountDnaSymbols(string dataset)
-		{
-			var result = new Dictionary<string, int>();
-
-			foreach (string dnaSymbol in _dnaSymbols)
-			{
-				int symbolCount = dataset.ToCharArray().Select(c => c.ToString()).Count(s => s == dnaSymbol);
-				result.Add(dnaSymbol, symbolCount);
-			}
-
-			return result;
-		}
-	}
 }
