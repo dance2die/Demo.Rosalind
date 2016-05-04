e5966c44a36b9324074fde2ec17ba8fedbd86870
dance2die <dontbothermewithspam@gmail.com>
2016-05-04 11:55:12 -0400

    FIB: test passes but answer not yet accepted.

--- Demo.Rosalind/Demo.Rosalind.Tests/FIB/FibTest.cs
+++ Demo.Rosalind/Demo.Rosalind.Tests/FIB/FibTest.cs
@@ -15,30 +15,43 @@ namespace Demo.Rosalind.Tests.FIB
 		}
 
 		[Theory]
-		[InlineData(1, 1, 5, 5)]
-		[InlineData(5, 3, 5, 19)]
-		public void TestFibonacciWithSequenceSpecified(int sequence1, int sequence2, int maxIterations, int expected)
+		[InlineData(5, 1, 5)]
+		[InlineData(5, 3, 19)]
+		public void TestFibonacciWithSequenceSpecified(int maxIterations, int rabbitPairs, int expected)
 		{
-			int actual = _sut.GetFibonacciSequence(sequence1, sequence2, maxIterations);
+			int actual = _sut.GetFibonacciSequence(maxIterations, rabbitPairs);
 
 			Assert.Equal(expected, actual);
 		}
 
+		[Fact]
+		public void ShowResult()
+		{
+			const int maxIterations = 29;
+			const int rabbitPairs = 3;
+
+			int result = _sut.GetFibonacciSequence(maxIterations, rabbitPairs);
+
+			_output.WriteLine(result.ToString());
+		}
 	}
 
 	public class Fib
 	{
-		public int GetFibonacciSequence(int sequence1, int sequence2, int maxIterations)
+		public int GetFibonacciSequence(int maxIterations, int rabbitPairs)
 		{
 			if (maxIterations <= 2) return 1;
 
-			int nextValue = sequence1 + sequence2;
+			int sequence1 = 1;
+			int sequence2 = 1;
+
+			int nextValue = (sequence1 * rabbitPairs) + sequence2;
 			for (int i = 3; i < maxIterations; i++)
 			{
 				sequence1 = sequence2;
 				sequence2 = nextValue;
 
-				nextValue = sequence1 + sequence2;
+				nextValue = (sequence1 * rabbitPairs) + sequence2;
 			}
 
 			return nextValue;
