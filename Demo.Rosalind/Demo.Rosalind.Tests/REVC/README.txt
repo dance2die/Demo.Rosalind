9aaba916b0ef1dbcc8a8a58fd240d988ca6028ed
dance2die <dontbothermewithspam@gmail.com>
2016-05-04 10:56:17 -0400

    REVC: Tests for sample dataset passes

---
+++ Demo.Rosalind/Demo.Rosalind.Tests/REVC/README.txt
@@ -0,0 +1,24 @@
+﻿http://rosalind.info/problems/revc/
+
+
+Complementing a Strand of DNA solved by 17779
+July 1, 2012, 8 p.m. by Rosalind Team Topics: String Algorithms
+← →
+The Secondary and Tertiary Structures of DNAclick to expand
+
+Problem
+
+In DNA strings, symbols 'A' and 'T' are complements of each other, as are 'C' and 'G'.
+
+The reverse complement of a DNA string ss is the string scsc formed by reversing the symbols of ss, then taking the complement of each symbol (e.g., the reverse complement of "GTCA" is "TGAC").
+
+Given: A DNA string ss of length at most 1000 bp.
+
+Return: The reverse complement scsc of ss.
+
+Sample Dataset
+
+AAAACCCGGT
+Sample Output
+
+ACCGGGTTTT
---
+++ Demo.Rosalind/Demo.Rosalind.Tests/REVC/ReverseComplementTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Demo.Rosalind.Tests.REVC
+{
+	public class ReverseComplementTest
+	{
+		private readonly ITestOutputHelper _output;
+		private const string SAMPLE_DATASET = "AAAACCCGGT";
+		private readonly ReverseComplement _sut;
+
+		public ReverseComplementTest(ITestOutputHelper output)
+		{
+			_output = output;
+			_sut = new ReverseComplement();
+		}
+
+		[Fact]
+		public void TestSampleDataset()
+		{
+			// from the web site http://rosalind.info/problems/rna/
+			const string expected = "ACCGGGTTTT";
+
+			string actual = _sut.ReverseComplementDataset(SAMPLE_DATASET);
+
+			Assert.Equal(expected, actual);
+		}
+	}
+
+	public class ReverseComplement
+	{
+		public string ReverseComplementDataset(string dataset)
+		{
+			var reverseComplementMap = new Dictionary<string, string>
+			{
+				{"A", "T"},
+				{"T", "A"},
+				{"C", "G"},
+				{"G", "C"},
+			};
+
+			var sequence =
+				from oneCharText in dataset.Reverse().Select(c => c.ToString())
+				select oneCharText.Replace(oneCharText, reverseComplementMap[oneCharText]);
+			return string.Join("", sequence);
+		}
+	}
+}
