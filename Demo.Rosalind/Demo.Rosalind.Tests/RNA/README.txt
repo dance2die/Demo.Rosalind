be59d2b0936a18fd6aafccbfb2ba0df059c00964
dance2die <dontbothermewithspam@gmail.com>
2016-05-04 10:19:09 -0400

    Rna.TranscribeDnaToRna satisfies the sample data test

---
+++ Demo.Rosalind/Demo.Rosalind.Tests/RNA/README.txt
@@ -0,0 +1,24 @@
+﻿http://rosalind.info/problems/rna/
+
+
+Transcribing DNA into RNA solved by 19548
+July 1, 2012, 8 p.m. by Rosalind Team Topics: String Algorithms
+← →
+The Second Nucleic Acidclick to expand
+
+Problem
+
+An RNA string is a string formed from the alphabet containing 'A', 'C', 'G', and 'U'.
+
+Given a DNA string tt corresponding to a coding strand, its transcribed RNA string uu is formed by replacing all occurrences of 'T' in tt with 'U' in uu.
+
+Given: A DNA string tt having length at most 1000 nt.
+
+Return: The transcribed RNA string of tt.
+
+Sample Dataset
+
+GATGGAACTTGACTACGTAAATT
+Sample Output
+
+GAUGGAACUUGACUACGUAAAUU
---
+++ Demo.Rosalind/Demo.Rosalind.Tests/RNA/RnaTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Demo.Rosalind.Tests.RNA
+{
+	public class RnaTest
+	{
+		private readonly ITestOutputHelper _output;
+		private const string SAMPLE_DATASET = "GATGGAACTTGACTACGTAAATT";
+		private readonly Rna _sut;
+
+		public RnaTest(ITestOutputHelper output)
+		{
+			_output = output;
+			_sut = new Rna();
+		}
+
+		[Fact]
+		public void TestSampleDataset()
+		{
+			// from the web site http://rosalind.info/problems/rna/
+			const string expected = "GAUGGAACUUGACUACGUAAAUU";
+
+			string actual = _sut.TranscribeDnaToRna(SAMPLE_DATASET);
+
+			Assert.Equal(expected, actual);
+		}
+	}
+
+	public class Rna
+	{
+		public string TranscribeDnaToRna(string dataset)
+		{
+			const char fromChar = 'T';
+			const char toChar = 'U';
+
+			StringBuilder buffer = new StringBuilder(dataset.Length);
+			foreach (char c in dataset)
+			{
+				char charToAppend = c;
+				if (c == fromChar)
+					charToAppend = toChar;
+
+				buffer.Append(charToAppend);
+			}
+
+			return buffer.ToString();
+		}
+	}
+}
