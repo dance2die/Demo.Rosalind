cbb00eae25abd191fde86e35bf7a5c606e89e46a
dance2die <dontbothermewithspam@gmail.com>
2016-05-05 07:41:54 -0400

    GC: added GCTest

---
+++ Demo.Rosalind/Demo.Rosalind.Tests/GC/GCTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Demo.Rosalind.Tests.GC
+{
+	public class GCTest
+	{
+		private readonly ITestOutputHelper _output;
+
+		public GCTest(ITestOutputHelper output)
+		{
+			_output = output;
+		}
+
+		[Theory]
+		public void TestParsingFastaFormat()
+		{
+			
+		}
+	}
+}
