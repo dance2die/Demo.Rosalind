42de0c03ece4b03dad2f84ee47e9e6792a4b53ca
dance2die <dontbothermewithspam@gmail.com>
2016-05-06 07:26:23 -0400

    GC: "TestParsingFastaFormat" passes

---
+++ Demo.Rosalind/Demo.Rosalind.Tests/GC/GCContentTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Demo.Rosalind.Tests.GC
+{
+	public class GCContentTest
+	{
+		private readonly ITestOutputHelper _output;
+		private readonly GCContent _sut;
+		const string SAMPLE_INPUT = @">Rosalind_6404
+CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCC
+TCCCACTAATAATTCTGAGG
+>Rosalind_5959
+CCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCT
+ATATCCATTTGTCAGCAGACACGC
+>Rosalind_0808
+CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGAC
+TGGGAACCTGCGGGCAGTAGGTGGAAT";
+
+		public GCContentTest(ITestOutputHelper output)
+		{
+			_output = output;
+			_sut = new GCContent();
+		}
+
+		[Fact]
+		public void TestParsingFastaFormat()
+		{
+			var expected = new Dictionary<string, string>
+			{
+				{">Rosalind_6404", "CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCCTCCCACTAATAATTCTGAGG"},
+				{">Rosalind_5959", "CCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCTATATCCATTTGTCAGCAGACACGC"},
+				{">Rosalind_0808", "CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGACTGGGAACCTGCGGGCAGTAGGTGGAAT"}
+			};
+
+			Dictionary<string, string> actual = _sut.ComputeGCContent(SAMPLE_INPUT);
+
+			Assert.True(actual.SequenceEqual(expected));
+		}
+	}
+
+	public class GCContent
+	{
+		public Dictionary<string, string> ComputeGCContent(string input)
+		{
+			Dictionary<string, string> result = new Dictionary<string, string>();
+
+			var splitted = input.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+			var currentKey = string.Empty;
+			// if a line starts with this, then it's a key
+			const string keyPrefix = ">";
+
+			foreach (var line in splitted)
+			{
+				if (line.StartsWith(keyPrefix))
+				{
+					currentKey = line;
+					result.Add(line, string.Empty);
+				}
+				else
+				{
+					result[currentKey] += line;
+				}
+			}
+
+			return result;
+		}
+	}
+}
