cca52a8e7d7ba8bf5ae43e567d983439cce964d8
dance2die <dontbothermewithspam@gmail.com>
2016-05-07 10:18:38 -0400

    HAMM: implemented "TestParsingTwoStringsToCompare"

--- Demo.Rosalind/Demo.Rosalind.Tests/HAMM/HammTest.cs
+++ Demo.Rosalind/Demo.Rosalind.Tests/HAMM/HammTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.Rosalind.Tests.HAMM
@@ -10,12 +11,32 @@ namespace Demo.Rosalind.Tests.HAMM
 	public class HammTest
 	{
 		private readonly ITestOutputHelper _output;
+		private readonly Hamm _sut;
+		private const string SAMPLE_INPUT = @"GAGCCTACTAACGGGAT
+CATCGTAATGACGGCCT";
 
 		public HammTest(ITestOutputHelper output)
 		{
 			_output = output;
+			_sut = new Hamm();
 		}
 
+		[Fact]
+		public void TestParsingTwoStringsToCompare()
+		{
+			var expected = new[] {"GAGCCTACTAACGGGAT", "CATCGTAATGACGGCCT"};
 
+			IEnumerable<string> actual = _sut.ParseInput(SAMPLE_INPUT);
+
+			Assert.True(expected.SequenceEqual(actual));
+		}
+	}
+
+	public class Hamm
+	{
+		public IEnumerable<string> ParseInput(string input)
+		{
+			return input.Split(new [] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+		}
 	}
 }
