44101fd5001cba016dc033a019449478c79cb0f1
dance2die <dontbothermewithspam@gmail.com>
2016-05-08 11:07:34 -0400

    SUBS: TestSampleDataSet passes

--- Demo.Rosalind/Demo.Rosalind.Tests/SUBS/SubsTest.cs
+++ Demo.Rosalind/Demo.Rosalind.Tests/SUBS/SubsTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -22,37 +24,45 @@ ATAT";
 		{
 			var expected = new Tuple<string, string>("GATATATGCATATACTT", "ATAT");
 
-			Tuple<string, string> actual = _sut.SeparateDataset(SAMPLE_DATASET);
+			Tuple<string, string> actual = _sut.SeparateInput(SAMPLE_DATASET);
 
 			Assert.True(expected.Equals(actual));
 		}
 
-		//[Fact]
-		//public void TestSampleDataSet()
-		//{
-		//	var expected = new[] {2, 4, 10};
+		[Fact]
+		public void TestSampleDataSet()
+		{
+			var expected = new[] { 2, 4, 10 };
 
-		//	IEnumerable<int> actual = _sut.GetSubstringCounts();
+			IEnumerable<int> actual = _sut.GetSubstringCounts(SAMPLE_DATASET);
 
-		//	Assert.True(expected.SequenceEqual(actual));
-		//}
+			Assert.True(expected.SequenceEqual(actual));
+		}
 	}
 
 	public class Subs
 	{
+		public IEnumerable<int> GetSubstringCounts(string input)
+		{
+			var tuple = SeparateInput(input);
+			string s = tuple.Item1;
+			string t = tuple.Item2;
+
+			for (int i = 0; i < s.Length - t.Length; i++)
+			{
+				if (s.Substring(i, t.Length) == t)
+					yield return i + 1;	// Result is 1-based.
+			}
+		}
+
 		/// <summary>
 		/// Separate the input into string "s" and the substring "t".
 		/// </summary>
 		/// <returns>A tuple where item1 is "s" and item2 is "t".</returns>
-		public Tuple<string, string> SeparateDataset(string input)
+		public Tuple<string, string> SeparateInput(string input)
 		{
 			string[] split = input.Split(new [] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
 			return new Tuple<string, string>(split[0], split[1]);
 		}
-
-		//public IEnumerable<int> GetSubstringCounts()
-		//{
-
-		//}
 	}
 }
