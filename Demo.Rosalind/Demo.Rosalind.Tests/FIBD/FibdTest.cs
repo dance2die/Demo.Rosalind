b002c150aa39f607962da1a712bfb2688c0c7883
dance2die <dontbothermewithspam@gmail.com>
2016-05-09 06:39:51 -0400

    FIBD: created FibdTest

---
+++ Demo.Rosalind/Demo.Rosalind.Tests/FIBD/FibdTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Demo.Rosalind.Tests.FIBD
+{
+	public class FibdTest
+	{
+		private readonly ITestOutputHelper _output;
+		private readonly Fibd _sut;
+
+		public FibdTest(ITestOutputHelper output)
+		{
+			_output = output;
+			_sut = new Fibd();
+		}
+	}
+
+	public class Fibd
+	{
+
+	}
+}
