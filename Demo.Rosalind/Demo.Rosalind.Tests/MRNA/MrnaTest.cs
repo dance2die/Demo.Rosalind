3b786dd19343d456941d77dc6c4fb385bfdcd3cd
dance2die <dontbothermewithspam@gmail.com>
2016-05-14 12:35:10 -0400

    MRNA: I can't figure out

--- Demo.Rosalind/Demo.Rosalind.Tests/MRNA/MrnaTest.cs
+++ Demo.Rosalind/Demo.Rosalind.Tests/MRNA/MrnaTest.cs
@@ -7,6 +7,13 @@ using Xunit.Abstractions;
 
 namespace Demo.Rosalind.Tests.MRNA
 {
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <remarks>
+	/// Can't figure out how sample data is converted to sample output.
+	/// http://recologia.com.br/2013/05/problema-rosalind-inferring-mrna-from-protein-mrna/
+	/// </remarks>
 	public class MrnaTest
 	{
 		private readonly ITestOutputHelper _output;
