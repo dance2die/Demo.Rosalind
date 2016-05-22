49acac9bc90861b4e02b98f76c75bdfff4fcaac1
dance2die <dontbothermewithspam@gmail.com>
2016-05-21 23:31:02 -0400

    MPRT: Successfully retrieves correct N-Glycosylation locations

--- Demo.Rosalind/Demo.Rosalind.Tests/MPRT/MprtTest.cs
+++ Demo.Rosalind/Demo.Rosalind.Tests/MPRT/MprtTest.cs
@@ -1,4 +1,9 @@
-﻿using Xunit.Abstractions;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace Demo.Rosalind.Tests.MPRT
 {
@@ -29,9 +34,52 @@ namespace Demo.Rosalind.Tests.MPRT
 			_sut = new Mprt();
 		}
 
+		[Fact]
+		public void TestNGetNGlycosylationLocations()
+		{
+			const string sampleInput = "MKNKFKTQEELVNHLKTVGFVFANSEIYNGLANAWDYGPLGVLLKNNLKNLWWKEFVTKQKDVVGLDSAIILNPLVWKASGHLDNFSDPLIDCKNCKARYRADKLIESFDENIHIAENSSNEEFAKVLNDYEISCPTCKQFNWTEIRHFNLMFKTYQGVIEDAKNVVYLRPETAQGIFVNFKNVQRSMRLHLPFGIAQIGKSFRNEITPGNFIFRTREFEQMEIEFFLKEESAYDIFDKYLNQIENWLVSACGLSLNNLRKHEHPKEELSHYSKKTIDFEYNFLHGFSELYGIAYRTNYDLSVHMNLSKKDLTYFDEQTKEKYVPHVIEPSVGVERLLYAILTEATFIEKLENDDERILMDLKYDLAPYKIAVMPLVNKLKDKAEEIYGKILDLNISATFDNSGSIGKRYRRQDAIGTIYCLTIDFDSLDDQQDPSFTIRERNSMAQKRIKLSELPLYLNQKAHEDFQRQCQK";
+
+			var expected = new List<int> {85, 118, 142, 306, 395};
+
+			var actual = _sut.GetNGlycosylationLocations(sampleInput);
+
+			Assert.True(expected.SequenceEqual(actual));
+		}
+
+		//[Theory]
+		//[InlineData("A2Z669", "./MPRT/A2Z669.fasta.txt", "")]
+		//[InlineData("B5ZC00", "./MPRT/B5ZC00.fasta.txt", "85 118 142 306 395")]
+		//[InlineData("P07204_TRBM_HUMAN", "./MPRT/P07204_TRBM_HUMAN.fasta.txt", "47 115 116 382 409")]
+		//[InlineData("P20840_SAG1_YEAST", "./MPRT/P20840_SAG1_YEAST.fasta.txt", "79 109 135 248 306 348 364 402 485 501 614")]
+		//public void TestNGlycosylationLocations(string uniprotId, string fastaFilePath, string expected)
+		//{
+		//	string inputText = File.ReadAllText(fastaFilePath);
+
+		//	string actual = _sut.GetNGlycosylationLocationString(uniprotId, inputText);
+
+		//	Assert.Equal(expected, actual);
+		//}
 	}
 
 	public class Mprt
 	{
+		private const string NGLYCOSYLATION_REGEX_PATTERN = "(?<NGrlyosylation>N[^P][ST][^P])";
+
+		public IEnumerable<int> GetNGlycosylationLocations(string input)
+		{
+			Regex regex = new Regex(NGLYCOSYLATION_REGEX_PATTERN, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+			MatchCollection matches = regex.Matches(input);
+			foreach (Match match in matches)
+			{
+				// Need to add 1 to because match index is 0 based
+				const int offset = 1;
+				yield return match.Index + offset;
+			}
+		}
+
+		public string GetNGlycosylationLocationString(string uniprotId, string inputText)
+		{
+			return string.Empty;
+		}
 	}
 }
