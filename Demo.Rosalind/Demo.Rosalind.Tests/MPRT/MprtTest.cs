bf7f8e7f75f81fc616fd492b8eef9fd6b8fd751b
dance2die <dontbothermewithspam@gmail.com>
2016-05-21 22:30:39 -0400

    MPRT: added more test cases

--- Demo.Rosalind/Demo.Rosalind.Tests/MPRT/MprtTest.cs
+++ Demo.Rosalind/Demo.Rosalind.Tests/MPRT/MprtTest.cs
@@ -2,6 +2,24 @@
 
 namespace Demo.Rosalind.Tests.MPRT
 {
+	/// <summary>
+	/// Test Scenarios
+	/// 
+	/// Download sample FASTA from www.uniprot.org.
+	///		Parse FASTA and find N-glycosylation motif locations in FASTA sequence
+	///		Combine found locations into a string.
+	/// 
+	/// Parse N-glycosylation motif
+	/// 
+	/// Parse Sample DataSet.
+	/// 
+	/// Build UniProt URL for FASTA File
+	/// 
+	/// Download FASTA files
+	///		Parse FASTA file
+	///		Find locations of N-glycosylation motifs
+	///		Combine locations into a string
+	/// </summary>
 	public class MprtTest : BaseTest
 	{
 		private readonly Mprt _sut;
@@ -10,23 +28,6 @@ namespace Demo.Rosalind.Tests.MPRT
 		{
 			_sut = new Mprt();
 		}
-
-		/*
-			Test Scenarios
-
-			Download sample FASTA from www.uniprot.org.
-				Parse FASTA and find N-glycosylation motif locations in FASTA sequence
-				Combine found locations into a string.
-
-			Parse N-glycosylation motif
-
-			Parse Sample DataSet.
-
-			Build UniProt URL for FASTA File
-
-			Download FASTA files
-				Parse FASTA file
-		*/
 
 	}
 
