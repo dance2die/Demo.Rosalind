308a551bfca8a42dec3d2cd1f9e5844134d1e65f
dance2die <dontbothermewithspam@gmail.com>
2016-05-23 06:46:35 -0400

    CONS: Initial import

---
+++ Demo.Rosalind/Demo.Rosalind.Tests/CONS/ConsTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit.Abstractions;
+
+namespace Demo.Rosalind.Tests.CONS
+{
+	public class ConsTest : BaseTest
+	{
+		private readonly Cons _sut;
+
+		public ConsTest(ITestOutputHelper output) : base(output)
+		{
+			_sut = new Cons();
+		}
+	}
+
+	public class Cons
+	{
+	}
+}
