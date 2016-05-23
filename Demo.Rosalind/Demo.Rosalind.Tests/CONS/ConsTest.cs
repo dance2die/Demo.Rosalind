d5cf1e421e8b55813334c556b83636ed5ede4322
dance2die <dontbothermewithspam@gmail.com>
2016-05-23 15:20:23 -0400

    CONS: Profile Matrix is populated correctly.

--- Demo.Rosalind/Demo.Rosalind.Tests/CONS/ConsTest.cs
+++ Demo.Rosalind/Demo.Rosalind.Tests/CONS/ConsTest.cs
@@ -48,14 +48,92 @@ ATGGCACT";
 
 			Assert.True(expected.SequenceEqual(actual));
 		}
+
+		/// <remarks>
+		/// A: 5 1 0 0 5 5 0 0
+		/// C: 0 0 1 4 2 0 6 1
+		/// G: 1 1 6 3 0 1 0 0
+		/// T: 1 5 0 0 0 1 1 6
+		/// </remarks>
+		[Fact]
+		public void TestProfileMatrixValues()
+		{
+			ProfileMatrix expected = new ProfileMatrix
+			{
+				A = new List<int> { 5, 1, 0, 0, 5, 5, 0, 0 },
+				C = new List<int> { 0, 0, 1, 4, 2, 0, 6, 1 },
+				G = new List<int> { 1, 1, 6, 3, 0, 1, 0, 0 },
+				T = new List<int> { 1, 5, 0, 0, 0, 1, 1, 6 }
+			};
+
+			ProfileMatrix actual = _sut.GetProfileMatrix(SAMPLE_DATASET);
+
+			Assert.True(expected.A.SequenceEqual(actual.A));
+			Assert.True(expected.C.SequenceEqual(actual.C));
+			Assert.True(expected.G.SequenceEqual(actual.G));
+			Assert.True(expected.T.SequenceEqual(actual.T));
+		}
 	}
 
 	public class Cons
 	{
+		public ProfileMatrix GetProfileMatrix(string fastaString)
+		{
+			ProfileMatrix result = new ProfileMatrix();
+
+			var dnaStrings = GetDnaStringsFromFasta(fastaString).ToList();
+			int columnCount = dnaStrings.First().Length;
+			result.InitializeMatrix(columnCount);
+
+			for (int colIndex = 0; colIndex < columnCount; colIndex++)
+			{
+				for (int rowIndex = 0; rowIndex < dnaStrings.Count; rowIndex++)
+				{
+					char currentChar = dnaStrings[rowIndex][colIndex];
+					switch (currentChar)
+					{
+						case 'A':
+							result.A[colIndex]++;
+							break;
+						case 'C':
+							result.C[colIndex]++;
+							break;
+						case 'G':
+							result.G[colIndex]++;
+							break;
+						case 'T':
+							result.T[colIndex]++;
+							break;
+					}
+				}
+			}
+
+			return result;
+		}
+
 		public IEnumerable<string> GetDnaStringsFromFasta(string fastaString)
 		{
 			FastaReader reader = new FastaReader();
 			return reader.GetDnaStrings(fastaString);
+		}
+	}
+
+	public class ProfileMatrix
+	{
+		public List<int> A { get; set; } = new List<int>();
+		public List<int> C { get; set; } = new List<int>();
+		public List<int> G { get; set; } = new List<int>();
+		public List<int> T { get; set; } = new List<int>();
+
+		public void InitializeMatrix(int columnCount)
+		{
+			for (int i = 0; i < columnCount; i++)
+			{
+				A.Add(0);
+				C.Add(0);
+				G.Add(0);
+				T.Add(0);
+			}
 		}
 	}
 }
