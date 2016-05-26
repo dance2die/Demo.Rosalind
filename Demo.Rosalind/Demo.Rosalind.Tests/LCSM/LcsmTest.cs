a52213fd963d1d48ef4f0d235d86e00e7cb4e11a
dance2die <dontbothermewithspam@gmail.com>
2016-05-26 10:43:40 -0400

    LCSM: added a test file.

---
+++ Demo.Rosalind/Demo.Rosalind.Tests/LCSM/LcsmTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Demo.Rosalind.Tests.LCSM
+{
+	public class LcsmTest : BaseTest
+	{
+		private readonly Lcsm _sut;
+
+		public LcsmTest(ITestOutputHelper output) : base(output)
+		{
+			_sut = new Lcsm();
+		}
+	}
+
+	public class Lcsm
+	{
+	}
+}
