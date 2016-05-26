b388c318574e91e525195a158f788379f1537346
dance2die <dontbothermewithspam@gmail.com>
2016-05-25 20:15:30 -0400

    SPLC: TestConvertingStringIntoProtein produces a correct sample output

---
+++ Demo.Rosalind/Demo.Rosalind.Tests/SPLC/README.txt
@@ -0,0 +1,27 @@
+﻿http://rosalind.info/problems/splc/
+
+
+RNA Splicing solved by 3413
+July 23, 2012, 8 p.m. by Rosalind Team Topics: String Algorithms
+← →
+Genes are Discontiguousclick to expand
+
+Problem
+
+After identifying the exons and introns of an RNA string, we only need to delete the introns and concatenate the exons to form a new string ready for translation.
+
+Given: A DNA string ss (of length at most 1 kbp) and a collection of substrings of ss acting as introns. All strings are given in FASTA format.
+
+Return: A protein string resulting from transcribing and translating the exons of ss. (Note: Only one solution will exist for the dataset provided.)
+
+Sample Dataset
+
+>Rosalind_10
+ATGGTCTACATAGCTGACAAACAGCACGTAGCAATCGGTCGAATCTCGAGAGGCATATGGTCACATGATCGGTCGAGCGTGTTTCAAAGTTTGCGCCTAG
+>Rosalind_12
+ATCGGTCGAA
+>Rosalind_15
+ATCGGTCGAGCGTGT
+Sample Output
+
+MVYIADKQHVASREAYGHMFKVCA
---
+++ Demo.Rosalind/Demo.Rosalind.Tests/SPLC/SplcTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rosalind.Lib;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Demo.Rosalind.Tests.SPLC
+{
+	public class SplcTest : BaseTest
+	{
+		private readonly Splc _sut;
+		private const string SAMPLE_DATASET = @">Rosalind_10
+ATGGTCTACATAGCTGACAAACAGCACGTAGCAATCGGTCGAATCTCGAGAGGCATATGGTCACATGATCGGTCGAGCGTGTTTCAAAGTTTGCGCCTAG
+>Rosalind_12
+ATCGGTCGAA
+>Rosalind_15
+ATCGGTCGAGCGTGT";
+
+		public SplcTest(ITestOutputHelper output) : base(output)
+		{
+			_sut = new Splc();
+		}
+
+		[Fact]
+		public void TestConvertingStringIntoProtein()
+		{
+			// Removed introns manually from the given input on the web.
+			const string dnaString = "ATGGTCTACATAGCTGACAAACAGCACGTAGCATCTCGAGAGGCATATGGTCACATGTTCAAAGTTTGCGCCTAG";
+
+			Rna rna = new Rna();
+			var rnaString = rna.ConvertDnaToRna(dnaString);
+			string actual = rna.ConvertRnaToProtein(rnaString);
+
+			_output.WriteLine(actual);
+			const string expected = "MVYIADKQHVASREAYGHMFKVCA";
+			Assert.Equal(expected, actual);
+		}
+	}
+
+	public class Splc
+	{
+	}
+}
