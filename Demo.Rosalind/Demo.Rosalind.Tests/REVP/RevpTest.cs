a3cd95404f6779d25309f3c1eca4a6ee22c41be2
dance2die <dontbothermewithspam@gmail.com>
2016-05-29 17:46:25 -0400

    REVP: Added a test class.

---
+++ Demo.Rosalind/Demo.Rosalind.Tests/REVP/RevpTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Demo.Rosalind.Tests.REVP
+{
+	public class RevpTest : BaseTest
+	{
+		private readonly Revp _sut = new Revp();
+		private const string SAMPLE_DATASET = @">Rosalind_24
+TCAATGCATGCGGGTCTATATGCAT";
+
+		public RevpTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class Revp
+	{
+	}
+}
