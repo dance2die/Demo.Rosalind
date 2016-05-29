ce1de9d2ca921932b94b5326387fbe24d4d2e43d
dance2die <dontbothermewithspam@gmail.com>
2016-05-29 15:23:39 -0400

    PERM: permutation yield returns values but not added to the result for some reason even after ".ToList()" call.

---
+++ Demo.Rosalind/Demo.Rosalind.Tests/PERM/PermTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Demo.Rosalind.Tests.PERM
+{
+	public class PermTest : BaseTest
+	{
+		private readonly Perm _sut;
+		private const int SAMPLE_DATASET = 3;
+
+		public PermTest(ITestOutputHelper output) : base(output)
+		{
+			_sut = new Perm();
+		}
+
+		[Fact]
+		public void TestSampleDataSetPermutation()
+		{
+			int[,] expected =
+			{
+				{1, 2, 3},
+				{1, 3, 2},
+				{2, 1, 3},
+				{2, 3, 1},
+				{3, 1, 2},
+				{3, 2, 1}
+			};
+
+			IEnumerable<int[]> actual = _sut.GetPermutations(SAMPLE_DATASET).ToList();
+
+			
+		}
+	}
+
+	public class Perm
+	{
+		public IEnumerable<int[]> GetPermutations(int permutationCount)
+		{
+			int[] permutationList = Enumerable.Range(1, permutationCount).ToArray();
+			List<int[]> result = GeneratePermutations(permutationList, 0, permutationCount - 1).ToList();
+
+			return result;
+		}
+
+		// http://stackoverflow.com/a/756083/4035
+		private IEnumerable<int[]> GeneratePermutations(int[] permutationList, int startCount, int permutationCount)
+		{
+			if (startCount == permutationCount)
+			{
+				yield return permutationList;
+			}
+			else
+			{
+				for (int i = startCount; i <= permutationCount; i++)
+				{
+					Swap(ref permutationList, startCount, i);
+					GeneratePermutations(permutationList, startCount + 1, permutationCount).ToList();
+					Swap(ref permutationList, startCount, i);
+				}
+				
+			}
+		}
+
+		// http://stackoverflow.com/a/2094316/4035
+		public static void Swap(ref int[] list, int index1, int index2)
+		{
+			int tmp = list[index1];
+			list[index1] = list[index2];
+			list[index2] = tmp;
+		}
+
+	}
+}
---
+++ Demo.Rosalind/Demo.Rosalind.Tests/PERM/README.txt
@@ -0,0 +1,28 @@
+﻿http://rosalind.info/problems/perm/
+
+
+Enumerating Gene Orders solved by 5509
+Aug. 1, 2012, 8 p.m. by Rosalind Team Topics: Combinatorics, Genome Rearrangements
+← →
+Rearrangements Power Large-Scale Genomic Changesclick to expand
+
+Problem
+
+A permutation of length nn is an ordering of the positive integers {1,2,…,n}{1,2,…,n}. For example, π=(5,3,2,1,4)π=(5,3,2,1,4) is a permutation of length 55.
+
+Given: A positive integer n≤7n≤7.
+
+Return: The total number of permutations of length nn, followed by a list of all such permutations (in any order).
+
+Sample Dataset
+
+3
+Sample Output
+
+6
+1 2 3
+1 3 2
+2 1 3
+2 3 1
+3 1 2
+3 2 1
