a8d729f765e3e79f0a8394cb6e345a86155d45cf
dance2die <dontbothermewithspam@gmail.com>
2016-05-29 16:34:23 -0400

    PERM: "TestSampleDataSetPermutation" passes.

--- Demo.Rosalind/Demo.Rosalind.Tests/PERM/PermTest.cs
+++ Demo.Rosalind/Demo.Rosalind.Tests/PERM/PermTest.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -21,18 +19,55 @@ namespace Demo.Rosalind.Tests.PERM
 		[Fact]
 		public void TestSampleDataSetPermutation()
 		{
-			int[,] expected =
+			List<int[]> expected = new List<int[]> 
 			{
-				{1, 2, 3},
-				{1, 3, 2},
-				{2, 1, 3},
-				{2, 3, 1},
-				{3, 1, 2},
-				{3, 2, 1}
+				new [] {1, 2, 3},
+				new [] {1, 3, 2},
+				new [] {2, 1, 3},
+				new [] {2, 3, 1},
+				new [] {3, 1, 2},
+				new [] {3, 2, 1}
 			};
 
-			IEnumerable<int[]> actual = _sut.GetPermutations(SAMPLE_DATASET).ToList();
-			
+			var actual = _sut.GetPermutations(SAMPLE_DATASET).ToList();
+			actual.Sort(new ArrayComparison());
+
+			Assert.True(IsMultidimensionalArraySequenceEqual(expected, actual));
+		}
+
+		private bool IsMultidimensionalArraySequenceEqual(List<int[]> list1, List<int[]> list2)
+		{
+			for (int i = 0; i < list1.Count; i++)
+			{
+				if (!list1[i].SequenceEqual(list2[i]))
+					return false;
+			}
+
+			return true;
+		}
+	}
+
+	public class ArrayComparison : IComparer<int[]>
+	{
+		public int Compare(int[] a1, int[] a2)
+		{
+			int sortValue1 = 0;
+			int sortValue2 = 0;
+			int powerCount = a1.Length - 1;
+			for (int i = 0; i < a1.Length; i++)
+			{
+				int power = (int)Math.Pow(10, powerCount);
+				sortValue1 += a1[i] * power;
+				sortValue2 += a2[i] * power;
+
+				powerCount--;
+			}
+
+			if (sortValue1 > sortValue2) return 1;
+			if (sortValue1 == sortValue2) return 0;
+			if (sortValue1 < sortValue2) return -1;
+
+			return 0;
 		}
 	}
 
@@ -64,7 +99,7 @@ namespace Demo.Rosalind.Tests.PERM
 					}
 					Swap(permutationList, startCount, i);
 				}
-				
+
 			}
 		}
 
